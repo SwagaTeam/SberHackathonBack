3d850136353806b387dd2e636bf85be43a81695e
younggogy <younggogy@gmail.com>
2025-11-22 15:47:57 +0500

    добавил сервисы

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace Domain.Entities;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; }   
+    public string Username { get; set; }
+    public string PasswordHash { get; set; }
+    public string Salt { get; set; }
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+    public string Role { get; set; }
+    public DateOnly Birthday { get; set; }
+    public string PhoneNumber { get; set; }
+    public int BooksInHand { get; set; }
+    public virtual ICollection<BorrowRecord> UserBooks { get; set; } = new List<BorrowRecord>();
+}
